ef308c875df4b23f7a088a7d2ad79761228b4182
Aaron Bushell <aaron_bushell@yahoo.com>
2021-05-12 21:40:51 +0100

    complete module 5 exercise 1

--- Day 3/Module 5 Exercises.py
+++ Day 3/Module 5 Exercises.py
@@ -1,61 +1,67 @@
-"""
-Module 5: Lists and Dictionaries
+# Module 5: Lists and Dictionaries
 
-1. Assume that the variable data refers to the list [5, 3, 7] Write the values of the following expressions:
+# 1. Assume that the variable data refers to the list [5, 3, 7] Write the values of the following expressions:
+data = [5, 3, 7]
 
-a. data[2]
+data[2]
+# 7
 
-b. data[-1]
+data[-1]
+# 7
 
-c. len(data)
+len(data)
+# 3
 
-d. data[0:2]
+data[0:2]
+# [5, 3]
 
-e. 0 in data
+0 in data
+# False
 
-f. data + [2, 10, 5]
+data + [2, 10, 5]
+# [5, 3, 7, 2, 10, 5]
 
-g. tuple(data)
+tuple(data)
+# (5, 3, 7) now a tuple
 
-2. Assume that the variable data refers to the list [5, 3, 7] Write the expressions that perform the following tasks:
+# 2. Assume that the variable data refers to the list [5, 3, 7] Write the expressions that perform the following tasks:
 
-a. Replace the value at position 0 in data with that value’s negation.
+# a. Replace the value at position 0 in data with that value’s negation.
 
-b. Add the value 10 to the end of data.
+# b. Add the value 10 to the end of data.
 
-c. Insert the value 22 at position 2 in data.
+# c. Insert the value 22 at position 2 in data.
 
-d. Remove the value at position 1 in data.
+# d. Remove the value at position 1 in data.
 
-3. Define a function named even. This function expects a number as an argument and returns True if the number is divisible by 2, or it returns False otherwise. (Hint: A number is evenly divisible by 2 if the remainder is 0.)
+# 3. Define a function named even. This function expects a number as an argument and returns True if the number is divisible by 2, or it returns False otherwise. (Hint: A number is evenly divisible by 2 if the remainder is 0.)
 
-4. Use the function even to create a similar funaction called odd
+# 4. Use the function even to create a similar funaction called odd
 
-5. Define a function named summation. This function expects two numbers, named low and high, as arguments. The function computes and returns the sum of the numbers between low and high, inclusive.
+# 5. Define a function named summation. This function expects two numbers, named low and high, as arguments. The function computes and returns the sum of the numbers between low and high, inclusive.
 
-6. Assume that the variable data refers to the dictionary: {'b':20, 'a':35} Write the values of the following expressions:
+# 6. Assume that the variable data refers to the dictionary: {'b':20, 'a':35} Write the values of the following expressions:
 
-a. data['a']
+data['a']
 
-b. data.get('c', None)
+data.get('c', None)
 
-c. len(data)
+len(data)
 
-d. data.keys( )
+data.keys( )
 
-e. data.values( )
+data.values( )
 
-f. data.pop('b')
+data.pop('b')
 
-g. data # After the pop above
+data # After the pop above
 
-7. Assume that the variable data refers to the dictionary: {'b':20, 'a':35} Write the expressions that perform the following tasks:
+# 7. Assume that the variable data refers to the dictionary: {'b':20, 'a':35} Write the expressions that perform the following tasks:
 
-a. Replace the value at the key 'b' with that value’s negation.
+# a. Replace the value at the key 'b' with that value’s negation.
 
-b. Add the key/value pair 'c':40 to data.
+# b. Add the key/value pair 'c':40 to data.
 
-c. Remove the value at key 'b' in data, safely.
+# c. Remove the value at key 'b' in data, safely.
 
-d. Print the keys in data in alphabetical order.
-"""
+# d. Print the keys in data in alphabetical order.
--- Day 3/working.py
+++ Day 3/working.py
@@ -1,8 +1,3 @@
-my_string = "banana"
-my_reversed_string = ""
-for index in range(len(my_string)-1, -1, -1):
-    my_reversed_string += my_string[index]
-print(my_string)
-print(my_reversed_string)
-
+data = [5, 3, 7]
+print(tuple(data))
 
